d37e71d872634d95e270ced8a2548f3163eba286
andrewquamme <andrewquamme@gmail.com>
2018-12-04 15:36:54 -0800

    added code to day 14 tictactoe

--- code-challenges/301/regex-part3/challenges-14.test.js
+++ code-challenges/301/regex-part3/challenges-14.test.js
@@ -179,8 +179,28 @@ Here is a sample board:
 ------------------------------------------------------------------------------------------------ */
 
 const detectTicTacToeWin = (board) => {
-  // Solution code here...
-}
+  //<solution>
+  const helpCheck = (row1, col1, row2, col2, row3, col3) => {
+
+    return board[row1][col1] !== '' && // make sure cell isn't empty
+      board[row1][col1] === board[row2][col2] &&
+      board[row2][col2] === board[row3][col3];
+  }
+
+  if(helpCheck(0,0,0,1,0,2)) return true; // top row
+  if(helpCheck(1,0,1,1,1,2)) return true; // middle row
+  if(helpCheck(2,0,2,1,2,2)) return true; // bottom row
+
+  if(helpCheck(0,0,1,0,2,0)) return true; // left column
+  if(helpCheck(0,1,1,1,2,1)) return true; // center column
+  if(helpCheck(0,2,1,2,2,2)) return true; // right column
+
+  if(helpCheck(0,0,1,1,2,2)) return true; // top left diagonal
+  if(helpCheck(0,2,1,1,2,0)) return true; // top right diagonal
+
+  return false;
+  //</solution>
+};
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
